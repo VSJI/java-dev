2e4e14523ed443c82936e898438ac6e3a608bb31
jivelasco <jivelasco@gmail.com>
2021-09-19 20:31:27 +0200

    Actualización de primera clase

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,40 +1,34 @@
 public class HolaMundo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String saludo = "Juan vas bien con Java";
-        System.out.println(saludo);
-        System.out.println("saludo.toUpperCase() = " + saludo.toUpperCase());
-        
-        int numero = 10;
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 11;
 
         boolean valor = true;
-        int numero2 = 6;
-        if (valor){
+        int numero2 = 5;
+        if(valor){
             System.out.println("numero = " + numero);
-            numero2 = 2;
+            numero2 = 10;
         }
         System.out.println("numero2 = " + numero2);
-        
+
         var numero3 = "15";
-        
+
         String nombre;
-        nombre = "Juan";
-        
-        if (numero > 9){
-            nombre = "John";
+
+        nombre = "Juan Velasco";
+
+        if(numero >10){
+            nombre = "Juan";
         }
 
         System.out.println("nombre = " + nombre);
 
         int edadPersona = 5;
     }
+
 }
-/*Un método tiene que ser público, statico, void(este metodo no devuelve valor, solo ejecuta) y main*/
 
-/*Tipos de datos: PrimitivosEnteros y referencia*/
-
-/*PrimitivosEnteros son booleanos, integer, float, etc*/
-
-/*Referencia son instancia a array, clases, interfaces, */
-
-/*soutp, soutv, psvm para crear clase desde cero*/
